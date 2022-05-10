be198d78721983d20eff6f50609367efb594b09f
Dimitris Paraskevopoulos <77070382+Dimipara@users.noreply.github.com>
2022-05-10 12:09:15 +0200

    Changed the naming of the inside folder
    Now it should default to using the search term as a folder name

--- google_images_download/google_images_download.py
+++ google_images_download/google_images_download.py
@@ -1064,10 +1064,10 @@ class googleimagesdownload:
                     if arguments['image_directory']:
                         dir_name = arguments['image_directory']
                     elif arguments['no_directory']:
-                        dir_name = search_term # THIS IS WHAT IS CHANGED. PREVIOUS VERSION: dir_name = ''
+                        dir_name = '' 
                     else:
-                        dir_name = search_term + (
-                            '-' + arguments['color'] if arguments['color'] else '')  # sub-directory
+                        dir_name = search_term      # THIS IS WHAT IS CHANGED. PREVIOUS VERSION: dir_name = search_term + ('-' + arguments['color'] if arguments['color'] else '') # sub-directory
+                           
 
                     if not arguments["no_download"]:
                         self.create_directories(main_directory, dir_name, arguments['thumbnail'],
